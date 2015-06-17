ff2a2b2fede61cae9b98575518b38ddb71a26691
Sd Barcellos <barcellos@cbm.sc.gov.br>
2015-06-17 11:10:42 -0300

    .

--- app/src/main/java/igarape/cbmsc/bombcast/views/Select_Vtr_Activity.java
+++ app/src/main/java/igarape/cbmsc/bombcast/views/Select_Vtr_Activity.java
@@ -22,22 +22,14 @@ import igarape.cbmsc.bombcast.R;
 import igarape.cbmsc.bombcast.utils.ConexaoHttpClient;
 import igarape.cbmsc.bombcast.utils.Globals;
 
-/**
- * Created by barcellos on 24/02/15.
- */
 public class Select_Vtr_Activity extends Activity {
 
-    private Spinner sp_vtrs;
     private List<String> vtrs = new ArrayList<>();
-    private List<String> cmta = new ArrayList<>();
     private String vtr_sel;
     private String servidor193 = Globals.getServidorSelecionado();
     private String usuario = Globals.getUserName();
-    private String status = null;
     EditText et_telefone;
     TextView nm_cmt;
-
-
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,17 +40,16 @@ public class Select_Vtr_Activity extends Activity {
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build(); StrictMode.setThreadPolicy(policy);
 
-
-        //Adicionando Nomes no ArrayList
+        String status;
         try {
             status = ConexaoHttpClient.executaHttpGet(Globals.SERVER_CBM + "sel_vtr.bombcast.php?u="+usuario+"&h="+servidor193);
             if(!status.isEmpty()){
                 vtrs = Arrays.asList(status.split("\\."));
             }else{
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setMessage("Não foi encontrada nenhuma viatura para o seu usuário. Verifique seu cadastro no e-193.")
+                builder.setMessage(getString(R.string.texto_vtr_nao_cadastrada))
                         .setCancelable(false)
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
                                 Intent intent2 = new Intent(Select_Vtr_Activity.this, LoginActivity.class);
                                 startActivity(intent2);
@@ -72,18 +63,10 @@ public class Select_Vtr_Activity extends Activity {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
-
         //Identifica o Spinner no layout
-        sp_vtrs = (Spinner) findViewById(R.id.sp_vtrs);
+        Spinner sp_vtrs = (Spinner) findViewById(R.id.sp_vtrs);
 
-        //Cria um ArrayAdapter usando um padrão de layout da classe R do android, passando o ArrayList vtrs
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item, vtrs);
-
-
-
-        ArrayAdapter<String> spinnerArrayAdapter = arrayAdapter;
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item, vtrs);
         spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_dropdown);
         sp_vtrs.setAdapter(spinnerArrayAdapter);
 
@@ -94,22 +77,20 @@ public class Select_Vtr_Activity extends Activity {
             public void onItemSelected(AdapterView<?> parent, View v, int posicao, long id) {
                 //pega nome pela posição
                 vtr_sel = parent.getItemAtPosition(posicao).toString();
-                //imprime um Toast na tela com o nome que foi selecionado
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
 
         try {
             status = ConexaoHttpClient.executaHttpGet(Globals.SERVER_CBM + "sel_vtr.bombcast.php?u="+usuario+"&h="+servidor193+"&vf=1");
             if(!status.isEmpty()){
-                cmta = Arrays.asList(status.split("\\."));
+                List<String> cmta = Arrays.asList(status.split("\\."));
 
-                String telefone = cmta.get(2).toString();
-                String idCmt = cmta.get(0).toString()+" "+cmta.get(1).toString();
+                String telefone = cmta.get(2);
+                String idCmt = cmta.get(0) +" "+ cmta.get(1);
 
                 ((EditText) findViewById(R.id.et_cel_cmt_area)).setText(telefone);
                 ((TextView) findViewById(R.id.tv_nm_cmt)).setText(idCmt);
@@ -137,13 +118,11 @@ public class Select_Vtr_Activity extends Activity {
             @Override
             public void onClick(View view) {
 
-
                 Globals.setTelefoneCmt(et_telefone.getText().toString());
                 Globals.setVtrSelecionada(vtr_sel);
 
                 Intent intent = new Intent(Select_Vtr_Activity.this, Ocorrencia_Activity.class);
                 startActivity(intent);
-
             }
         });
 
@@ -172,16 +151,11 @@ public class Select_Vtr_Activity extends Activity {
             }
         });
 
-
+        //Precisa arrumar uma forma de envio -- sem uso no momento
         final Button btn_upload = (Button) findViewById(R.id.btn_upload);
         btn_upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-               // Intent intent2 = new Intent(Select_Vtr_Activity.this, UploadToServer.class);
-               // startActivity(intent2);
-               // Select_Vtr_Activity.this.finish();
-
             }
         });
 
@@ -189,11 +163,6 @@ public class Select_Vtr_Activity extends Activity {
 
     @Override
     protected void onDestroy() {
-
-      //  Intent intent = new Intent(Select_Vtr_Activity.this, UploadService.class);
-      //  intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-      //  stopService(intent);
-
         super.onDestroy();
     }
 }
